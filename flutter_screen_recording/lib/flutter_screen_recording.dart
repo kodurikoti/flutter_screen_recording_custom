03a2a9ab727dec8b9c568afd7fee6f1ee8eb1d3a
koti <pccs.skyline@gmail.com>
2025-09-18 14:35:06 +0530

    stopRecordScreenKeepService custom method adding eventAction: ForegroundTaskEventAction.none,

--- flutter_screen_recording/lib/flutter_screen_recording.dart
+++ flutter_screen_recording/lib/flutter_screen_recording.dart
@@ -104,6 +104,7 @@ class FlutterScreenRecording {
             // interval: 5000,
             autoRunOnBoot: true,
             allowWifiLock: true,
+            eventAction: ForegroundTaskEventAction.none,
           ),
         );
       }
